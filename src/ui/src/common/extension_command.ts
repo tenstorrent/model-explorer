26f398eb6ab1c9f693e9189dd132bd89d247db35
madcampos <mcampos@tenstorrent.com>
2025-05-13 16:12:39 +0000

    feat: narrow type for execute command settings

--- src/ui/src/common/extension_command.ts
+++ src/ui/src/common/extension_command.ts
@@ -76,9 +76,17 @@ export type AdapterOverrideResponse = ExtensionResponse<[{
   success: boolean;
 }], never>;
 
+/** Adapter's "execute" settings. */
+export declare interface AdapterExecuteSettings {
+  optimizationPolicy: string;
+  genCppCode: boolean;
+  overrides?: OverridesPerNode;
+}
+
 /** Adapter's "execute" command. */
 export declare interface AdapterExecuteCommand extends ExtensionCommand {
   cmdId: 'execute';
+  settings: AdapterExecuteSettings
 }
 
 /** Adapter's "execute" results inside the response. */
