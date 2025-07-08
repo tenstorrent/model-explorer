8f874a36de4a5970790f8b72be6021fa1278253a
Marco Campos <mcampos@tenstorrent.com>
2025-07-08 23:03:27 +0000

    feat: send event after reset

--- src/ui/src/components/visualizer/model_graph_visualizer.ts
+++ src/ui/src/components/visualizer/model_graph_visualizer.ts
@@ -16,6 +16,12 @@
  * ==============================================================================
  */
 
+declare global {
+	interface DocumentEventMap {
+		'visualizer-reset': CustomEvent<undefined>;
+	}
+}
+
 import {CommonModule} from '@angular/common';
 import {
   ChangeDetectionStrategy,
@@ -358,6 +364,8 @@ export class ModelGraphVisualizer implements OnInit, OnDestroy, OnChanges {
     this.appService.reset();
     this.uiStateService.reset();
     this.ngOnInit();
+
+    requestAnimationFrame(() => document.dispatchEvent(new CustomEvent<undefined>('visualizer-reset')));
   }
 
   @HostListener('document:keydown', ['$event'])
