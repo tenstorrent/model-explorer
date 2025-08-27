79e96bbdf5f4f8252897857f370d440dced1473b
Marco Campos <mcampos@tenstorrent.com>
2025-08-27 14:00:36 +0000

    chore: remove leftovers from testing

--- src/server/package/src/model_explorer/extension_manager.py
+++ src/server/package/src/model_explorer/extension_manager.py
@@ -80,7 +80,6 @@ class ExtensionManager(object, metaclass=Singleton):
         'id': 'builtin_json',
         'name': 'JSON adapter',
         'description': 'Convert graphs json data file or tfjs model.',
-        'enableExecution': False,
     })
     return exts
 
