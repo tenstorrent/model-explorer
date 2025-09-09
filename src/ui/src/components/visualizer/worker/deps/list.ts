fccdb177ac772219c114e9dfb0d3c272d0743ab1
Marco Campos <mcampos@tenstorrent.com>
2025-09-09 18:51:52 +0000

    feat: move list to it's own file

---
+++ src/ui/src/components/visualizer/worker/deps/list.ts
@@ -0,0 +1,67 @@
+interface Sentinel {
+  _next?: Sentinel;
+  _prev?: Sentinel;
+}
+
+/*
+ * Simple doubly linked list implementation derived from Cormen, et al.,
+ * "Introduction to Algorithms".
+ */
+export class List {
+  _sentinel: Sentinel;
+
+  constructor() {
+    let sentinel: Sentinel = {};
+    sentinel._next = sentinel._prev = sentinel;
+    this._sentinel = sentinel;
+  }
+
+  dequeue() {
+    let sentinel = this._sentinel;
+    let entry = sentinel._prev;
+
+    if (entry !== sentinel) {
+      unlink(entry);
+      return entry;
+    }
+
+    return;
+  }
+
+  enqueue(entry: any & Sentinel) {
+    let sentinel = this._sentinel;
+    if (entry._prev && entry._next) {
+      unlink(entry);
+    }
+    entry._next = sentinel._next;
+    if (sentinel._next) {
+      sentinel._next._prev = entry;
+    }
+    sentinel._next = entry;
+    entry._prev = sentinel;
+  }
+
+  toString() {
+    let strs = [];
+    let sentinel = this._sentinel;
+    let curr = sentinel._prev;
+    while (curr !== sentinel) {
+      strs.push(JSON.stringify(curr, filterOutLinks));
+      curr = curr?._prev;
+    }
+    return '[' + strs.join(', ') + ']';
+  }
+}
+
+function unlink(entry: any & Sentinel) {
+  entry._prev._next = entry._next;
+  entry._next._prev = entry._prev;
+  delete entry._next;
+  delete entry._prev;
+}
+
+function filterOutLinks(k: string, v: any) {
+  if (k !== '_next' && k !== '_prev') {
+    return v;
+  }
+}
